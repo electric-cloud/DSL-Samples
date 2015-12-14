a624f30ce2710a931f1c354b977315c06bee8255
Greg Maxey <gmaxey@electric-cloud.com>
2015-12-14 17:21:27 -0500

    Use pluginKey instead of pluginName which includes version number

--- Release Model/createAppModel.groovy
+++ Release Model/createAppModel.groovy
@@ -46,7 +46,8 @@ project "Default", {
 			// Create artifact
 			artifact groupId: artifactGroup, artifactKey: artifactKey
 		
-			component componentName: artifactKey, pluginName: getPlugin(pluginName: "EC-Artifact").pluginName, {
+			//component componentName: artifactKey, pluginName: getPlugin(pluginName: "EC-Artifact").pluginName, {
+			component componentName: artifactKey, pluginKey: "EC-Artifact", {
 				ec_content_details.with { 
 					pluginProjectName = "EC-Artifact"
 					pluginProcedure = "Retrieve"
